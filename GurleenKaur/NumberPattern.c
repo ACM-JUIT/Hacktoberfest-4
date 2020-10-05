b93120a2323ac085748e2be9289102312dfb9469
neeti153 <72305939+neeti153@users.noreply.github.com>
2020-10-05 20:33:33 +0530

    Update NumberPattern.c

--- GurleenKaur/NumberPattern.c
+++ GurleenKaur/NumberPattern.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main()
+{
+   int n=5,i,j;
+
+   for(i=5;i>=1;i--)
+   {
+      for(j=1;j<=5;j++)
+      {
+         printf("%d",i);
+      }
+      printf("\n");
+   }
+   return 0;
+}
